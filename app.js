e047ef09f0d019c93f047baa59969194272773b0
Aakash Walji <aakashwalji@gmail.com>
2024-01-22 18:24:47 +0530

    added changes

---
+++ app.js
@@ -0,0 +1,9 @@
+function getId(id){
+    document.getElementById(id);
+}
+
+// let tab = document.querySelectorAll("tab");
+// const showContent = (name) => {
+//      id(name).style.display = "block";
+// }
+getId("shop").style.display = "block";
